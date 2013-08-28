d916ad15c820b80eab2198b7a362822452bfb9c0
Jacopo Sabbatini <dedalusj@gmail.com>
2013-08-29 06:47:53 +1000

    Cleaned up the mainController code

--- webapp/keepupwithscience/frontend/static/js/main/controllers/mainController.js
+++ webapp/keepupwithscience/frontend/static/js/main/controllers/mainController.js
@@ -6,18 +6,6 @@ app.controller("mainController", function($scope, $http, $resource){
     var Category = $resource('http://localhost:port/api/categories/:categoryId', {categoryId:'@id', port: ':5000'});
     
     $scope.init = function() {
-        $scope.categories = Category.query(function() {
-        
-        });
-//        var q = {order_by: [{field: "name", direction: "asc"}]};
-//        $http.get('http://localhost:5000/api/categories').success(function(data, status, headers, config) {
-//            As we are getting our data from an external source, we need to format the data so we can use it to our desired affect
-//            For each day get all the episodes
-//            angular.forEach(data.categories, function(category, index){
-//                $scope.categories.push(category);
-//            });
-//        }).error(function(data, status, headers, config) {
-//
-//        });
+        $scope.categories = Category.query();
     };
 });
