88dc0d3e2e097b30e120ac035f3d59bbe543e02c
Jacopo Sabbatini <dedalusj@gmail.com>
2013-08-28 22:38:21 +1000

    Use angularJS resource foe the categories

--- webapp/keepupwithscience/frontend/static/js/main/controllers/mainController.js
+++ webapp/keepupwithscience/frontend/static/js/main/controllers/mainController.js
@@ -1,25 +1,23 @@
-app.controller("mainController", function($scope, $http){
+app.controller("mainController", function($scope, $http, $resource){
  
     $http.defaults.headers.common['Authorization'] = 'Basic ' + Base64.encode('dedalusj@gmail.com:idathik');
+    
     $scope.categories = [];
+    var Category = $resource('http://localhost:port/api/categories/:categoryId', {categoryId:'@id', port: ':5000'});
+    
     $scope.init = function() {
-        var q = {order_by: [{field: "name", direction: "asc"}]};
-        $http.jsonp('http://localhost:5000/api/categories?q=' + angular.toJson(q) + '&callback=JSON_CALLBACK').success(function(data, status, headers, config) {
-//            console.log(headers);
-//            console.log(status);
-//            console.log(data);
-//            console.log(config);
+        $scope.categories = Category.query(function() {
         
-            //As we are getting our data from an external source, we need to format the data so we can use it to our desired affect
-            //For each day get all the episodes
-            angular.forEach(data.objects, function(category, index){
-                $scope.categories.push(category);
-            });
-        }).error(function(data, status, headers, config) {
-            console.log(headers);
-            console.log(status);
-            console.log(data);
-            console.log(config);
         });
+//        var q = {order_by: [{field: "name", direction: "asc"}]};
+//        $http.get('http://localhost:5000/api/categories').success(function(data, status, headers, config) {
+//            As we are getting our data from an external source, we need to format the data so we can use it to our desired affect
+//            For each day get all the episodes
+//            angular.forEach(data.categories, function(category, index){
+//                $scope.categories.push(category);
+//            });
+//        }).error(function(data, status, headers, config) {
+//
+//        });
     };
 });
--- webapp/keepupwithscience/frontend/static/js/main/main.js
+++ webapp/keepupwithscience/frontend/static/js/main/main.js
@@ -1,1 +1,1 @@
-var app = angular.module('KUWSApp',[]);
+var app = angular.module('KUWSApp',["ngResource"]);
