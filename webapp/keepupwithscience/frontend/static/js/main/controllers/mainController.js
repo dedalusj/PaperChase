bded29d0d6dfffb8538a06ad769945cde9c80602
Jacopo Sabbatini <dedalusj@gmail.com>
2013-09-02 06:42:42 +1000

    Created utility methods in the controller to fetch subcategories and journals and improved UI

--- webapp/keepupwithscience/frontend/static/js/main/controllers/mainController.js
+++ webapp/keepupwithscience/frontend/static/js/main/controllers/mainController.js
@@ -3,7 +3,17 @@ app.controller("mainController", function($scope, $http, $resource){
     $http.defaults.headers.common['Authorization'] = 'Basic ' + Base64.encode('dedalusj@gmail.com:idathik');
     
     $scope.categories = [];
-    var Category = $resource('http://localhost:port/api/categories/:categoryId', {categoryId:'@id', port: ':5000'});
+    $scope.subcategories = [];
+    $scope.journals = [];
+    var Category = $resource('http://localhost\\:5000/api/categories/:categoryId/:resource', {categoryId:'@id', resource: '@res'});
+    
+    $scope.updateSubcategories = function($event, categoryId) {
+        $scope.subcategories = Category.query({categoryId: categoryId, resource: 'subcategories'})
+    }
+    
+    $scope.updateJournals = function($event, categoryId) {
+        $scope.journals = Category.query({categoryId: categoryId, resource: 'journals'})
+    }
     
     $scope.init = function() {
         $scope.categories = Category.query();
