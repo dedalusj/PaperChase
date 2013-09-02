2d180e92da793d69f8555915ea02b463133db32a
Jacopo Sabbatini <dedalusj@gmail.com>
2013-09-02 22:05:52 +1000

    Fixed a bug with the categories selection method

--- webapp/keepupwithscience/frontend/static/js/main/controllers/mainController.js
+++ webapp/keepupwithscience/frontend/static/js/main/controllers/mainController.js
@@ -7,13 +7,25 @@ app.controller("mainController", function($scope, $http, $resource){
     $scope.journals = [];
     var Category = $resource('http://localhost\\:5000/api/categories/:categoryId/:resource', {categoryId:'@id', resource: '@res'});
     
+    $scope.selectedCategoryIndex = -1;
+    $scope.categoryClass = function(categoryId) {
+        return categoryId === $scope.selectedCategoryIndex ? 'active' : undefined;
+    };
     $scope.updateSubcategories = function($event, categoryId) {
-        $scope.subcategories = Category.query({categoryId: categoryId, resource: 'subcategories'})
-    }
+        $scope.subcategories = Category.query({categoryId: categoryId, resource: 'subcategories'});
+        $scope.journals = [];
+        $scope.selectedCategoryIndex = categoryId;
+        $scope.selectedSubcategoryIndex = -1;
+    };
     
+    $scope.selectedSubcategoryIndex = -1;
+    $scope.subcategoryClass = function(categoryId) {
+        return categoryId === $scope.selectedSubcategoryIndex ? 'active' : undefined;
+    };
     $scope.updateJournals = function($event, categoryId) {
-        $scope.journals = Category.query({categoryId: categoryId, resource: 'journals'})
-    }
+        $scope.journals = Category.query({categoryId: categoryId, resource: 'journals'});
+        $scope.selectedSubcategoryIndex = categoryId;
+    };
     
     $scope.init = function() {
         $scope.categories = Category.query();
