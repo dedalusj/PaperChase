d3e2045f595a746c76c22fdc29745bfb831ab93c
Jacopo Sabbatini <dedalusj@gmail.com>
2013-09-10 20:06:51 +1000

    Created Base64 and Auth services

--- webapp/keepupwithscience/frontend/static/js/services/services.js
+++ webapp/keepupwithscience/frontend/static/js/services/services.js
@@ -1,8 +1,111 @@
-app.factory('CategoryAPI', function($http, $resource){
+app.factory('Base64', function() {
+    var keyStr = 'ABCDEFGHIJKLMNOP' +
+        'QRSTUVWXYZabcdef' +
+        'ghijklmnopqrstuv' +
+        'wxyz0123456789+/' +
+        '=';
+    return {
+        encode: function (input) {
+            var output = "";
+            var chr1, chr2, chr3 = "";
+            var enc1, enc2, enc3, enc4 = "";
+            var i = 0;
+ 
+            do {
+                chr1 = input.charCodeAt(i++);
+                chr2 = input.charCodeAt(i++);
+                chr3 = input.charCodeAt(i++);
+ 
+                enc1 = chr1 >> 2;
+                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                enc4 = chr3 & 63;
+ 
+                if (isNaN(chr2)) {
+                    enc3 = enc4 = 64;
+                } else if (isNaN(chr3)) {
+                    enc4 = 64;
+                }
+ 
+                output = output +
+                    keyStr.charAt(enc1) +
+                    keyStr.charAt(enc2) +
+                    keyStr.charAt(enc3) +
+                    keyStr.charAt(enc4);
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+            } while (i < input.length);
+ 
+            return output;
+        },
+ 
+        decode: function (input) {
+            var output = "";
+            var chr1, chr2, chr3 = "";
+            var enc1, enc2, enc3, enc4 = "";
+            var i = 0;
+ 
+            // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+            var base64test = /[^A-Za-z0-9\+\/\=]/g;
+            if (base64test.exec(input)) {
+                alert("There were invalid base64 characters in the input text.\n" +
+                    "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+                    "Expect errors in decoding.");
+            }
+            input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+ 
+            do {
+                enc1 = keyStr.indexOf(input.charAt(i++));
+                enc2 = keyStr.indexOf(input.charAt(i++));
+                enc3 = keyStr.indexOf(input.charAt(i++));
+                enc4 = keyStr.indexOf(input.charAt(i++));
+ 
+                chr1 = (enc1 << 2) | (enc2 >> 4);
+                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                chr3 = ((enc3 & 3) << 6) | enc4;
+ 
+                output = output + String.fromCharCode(chr1);
+ 
+                if (enc3 != 64) {
+                    output = output + String.fromCharCode(chr2);
+                }
+                if (enc4 != 64) {
+                    output = output + String.fromCharCode(chr3);
+                }
+ 
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+ 
+            } while (i < input.length);
+ 
+            return output;
+        }
+    };
+});
+
+app.factory('Auth', function ($http, Base64) {
+//app.factory('Auth', function ($cookieStore, $http) {
+//    initialize to whatever is in the cookie, if anything
+//    $http.defaults.headers.common['Authorization'] = 'Basic ' + $cookieStore.get('authdata');
+    
+    return {
+        setCredentials: function (username, password) {
+            var encoded = Base64.encode(username + ':' + password);
+            $http.defaults.headers.common.Authorization = 'Basic ' + encoded;
+//            $cookieStore.put('authdata', encoded);
+        },
+        clearCredentials: function () {
+            document.execCommand("ClearAuthenticationCache");
+            $http.defaults.headers.common.Authorization = 'Basic ';
+//            $cookieStore.remove('authdata');
+        }
+    };
+});
+
+app.factory('CategoryAPI', function($http, $resource, Auth){
     // Define the resource for the category API to be shared by all other services and controllers
     
-    // TODO: this http header line for authentication should be probably moved to its own service
-    $http.defaults.headers.common['Authorization'] = 'Basic ' + Base64.encode('dedalusj@gmail.com:idathik');
+    Auth.setCredentials('dedalusj@gmail.com','idathik');
 	
 	return $resource('http://localhost\\:5000/api/categories/:categoryId/:resource', {categoryId:'@id', resource: '@res'});
 });
