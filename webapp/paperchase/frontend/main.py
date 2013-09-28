5b18762f9f7994f2880add8fc4cb703f3fbd9285
Jacopo Sabbatini <dedalusj@gmail.com>
2013-09-28 12:03:11 +1000

    Switched to flask generated assests for css and js

--- webapp/paperchase/frontend/main.py
+++ webapp/paperchase/frontend/main.py
@@ -1,9 +1,8 @@
-from flask import Blueprint, render_template, make_response
+from flask import Blueprint, render_template
 
 bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def index():
     """Returns the main interface."""
-    return make_response(open('paperchase/frontend/templates/main.html').read())
-#    return render_template('main.html')
+    return render_template('main.html')
