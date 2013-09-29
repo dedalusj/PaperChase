a87cd1a8a9b0870436430b765f904df0a9cd735b
Jacopo Sabbatini <dedalusj@gmail.com>
2013-09-29 16:00:34 +1000

    Updated fabfile methods

--- webapp/fabfile.py
+++ webapp/fabfile.py
@@ -1,6 +1,6 @@
-from fabric.api import local
+from fabric.api import *
 
-env.user = 'dedalus'
+env.user = 'paperchase'
 
 def inside():
     env.hosts = ['brick.local']
@@ -8,5 +8,37 @@ def inside():
 def outside():
     env.hosts = ['dedalusj.dyndns.org']
 
-def run_celery():
-    local("celery -A paperchase.tasks worker -B --loglevel=debug")
+def run_celery(loglevel="info"):
+    local("celery -A paperchase.tasks worker -B --loglevel=%s" % loglevel)    
+        
+def setup_mysql():
+    # setup mysql user and database here
+    pass
+
+def setup_redis():
+    # setup redis
+    pass
+    
+def setup_repo():
+    if run("git clone https://github.com/dedalusj/PaperChase.git .").failed:
+        run("git pull")
+
+def setup_venv():
+    run("curl -O https://raw.github.com/pypa/virtualenv/1.9.X/virtualenv.py")
+    run("python virtualenv.py PC")
+    run("PC/bin/pip install -r requirements.txt")
+
+def setup():
+    code_dir = '/Users/paperchase/server'
+    with cd(code_dir):
+        setup_mysql()
+        setup_redis()
+        setup_repo()
+    with cd(code_dir+'/webapp'):
+        setup_venv()
+        run("mkdir log")    
+    
+def deploy():
+    code_dir = '/Users/paperchase/server/webapp'
+    with cd(code_dir):
+        pass
