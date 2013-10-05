3a1ea11265c7ef0e346b43353d407996f4b2f0b5
Jacopo Sabbatini <dedalusj@gmail.com>
2013-10-05 22:30:41 +1000

    Styling the frontend for showing papers

--- webapp/paperchase/frontend/static/js/controllers/home.js
+++ webapp/paperchase/frontend/static/js/controllers/home.js
@@ -1,5 +1,11 @@
 app.controller("homeController", ['$scope', 'PaperServices', function($scope, PaperServices) {
-    $scope.papers = PaperServices.getPapers();
+    $scope.papers = PaperServices.getPapers(function(papers, getResponseHeaders){
+      for (var i=0;i<papers.length;i++)
+      { 
+          papers[i].created = new Date(papers[i].created);
+          console.log(papers[i]);
+      }
+    });
     
     $scope.selectedPaperId = undefined;
     $scope.isActive = function(paperId) {
