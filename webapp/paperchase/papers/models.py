26ddec825888abf55efdeb26963a7b67765124a8
Jacopo Sabbatini <dedalusj@gmail.com>
2013-10-05 12:16:29 +1000

    Added init method to Paper model to create a user_paper entry from every user subscribing to the journal

--- webapp/paperchase/papers/models.py
+++ webapp/paperchase/papers/models.py
@@ -5,8 +5,10 @@
 
     papers models
 """
+from sqlalchemy import event
 
 from ..core import db
+from ..models import Journal
 
 class Paper(db.Model):
     __tablename__ = 'papers'
@@ -23,3 +25,21 @@ class Paper(db.Model):
     
     def __str__(self):
             return self.title
+            
+    def __init__(self, **kwargs):
+        super(Paper, self).__init__( **kwargs )
+        journal = Journal.query.get(kwargs['journal_id'])
+        users = journal.users
+        for user in users:
+            user_paper = UserPaper(score=50)
+            user_paper.paper = self
+            user.papers.append(user_paper)
+            
+class UserPaper(db.Model):
+    __tablename__ = 'userpapers'
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+    paper_id = db.Column(db.Integer, db.ForeignKey('papers.id'), primary_key=True)
+    paper = db.relationship("Paper", backref=db.backref('user_paper'))    
+    read_at = db.Column(db.DateTime)
+    score = db.Column(db.Integer)
