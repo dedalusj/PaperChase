229dd45eae1eb58ca7f8a5e1de68ecdc6e9f540e
Jacopo Sabbatini <dedalusj@gmail.com>
2013-10-17 10:54:26 +1100

    Added sql file to bootstrap database

---
+++ webapp/database_bootstrap.sql
@@ -0,0 +1,103 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 127.0.0.1 (MySQL 5.6.12)
+# Database: development
+# Generation Time: 2013-10-15 09:53:52 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table journals
+# ------------------------------------------------------------
+
+LOCK TABLES `journals` WRITE;
+/*!40000 ALTER TABLE `journals` DISABLE KEYS */;
+
+INSERT INTO `journals` (`id`, `title`, `short_title`, `url`, `last_checked`, `next_check`, `metadata_update`, `favicon`, `color`)
+VALUES
+	(1,'Physical Review Letter','PRL','http://feeds.aps.org/rss/recent/prl.xml','2013-01-01 00:00:00','2013-01-01 00:00:00','2013-01-01 00:00:00','http://publish.aps.org/favicon.ico',NULL),
+	(2,'Nature','Nature','http://feeds.nature.com/NatureLatestResearch','2013-01-01 00:00:00','2013-01-01 00:00:00','2013-01-01 00:00:00','http://www.nature.com/favicon.ico',NULL),
+	(3,'arXiv Quantum Gases','arXiv:cond-mat.quant-gas','http://export.arxiv.org/rss/cond-mat.quant-gas','2013-01-01 00:00:00','2013-01-01 00:00:00','2013-01-01 00:00:00','http://arxiv.org/favicon.ico',NULL),
+	(4,'arXiv Disordered Systems and Neural Networks','arXiv:cond-mat.dis-nn','http://export.arxiv.org/rss/cond-mat.dis-nn/','2013-01-01 00:00:00','2013-01-01 00:00:00','2013-01-01 00:00:00','http://arxiv.org/favicon.ico',NULL),
+	(5,'arXiv Material Science','arXiv:cond-mat.mtrl-sci','http://export.arxiv.org/rss/cond-mat.mtrl-sci','2013-01-01 00:00:00','2013-01-01 00:00:00','2013-01-01 00:00:00','http://arxiv.org/favicon.ico',NULL),
+	(6,'arXiv Mesoscale and Nanoscale Physics','arXiv:cond-mat.mes-hall','http://export.arxiv.org/rss/cond-mat.mes-hall/','2013-01-01 00:00:00','2013-01-01 00:00:00','2013-01-01 00:00:00','http://arxiv.org/favicon.ico',NULL),
+	(7,'arXiv Other Condensed Matter','arXiv:cond-mat.other','http://export.arxiv.org/rss/cond-mat.other/','2013-01-01 00:00:00','2013-01-01 00:00:00','2013-01-01 00:00:00','http://arxiv.org/favicon.ico',NULL),
+	(8,'arXiv Soft Condensed Matter','arXiv:cond-mat.soft','http://export.arxiv.org/rss/cond-mat.soft/','2013-01-01 00:00:00','2013-01-01 00:00:00','2013-01-01 00:00:00','http://arxiv.org/favicon.ico',NULL),
+	(9,'arXiv Statistical Mechanics','arXiv:cond-mat.stat-mech','http://export.arxiv.org/rss/cond-mat.stat-mech','2013-01-01 00:00:00','2013-01-01 00:00:00','2013-01-01 00:00:00','http://arxiv.org/favicon.ico',NULL),
+	(10,'arXiv Superconductivity','arXiv:cond-mat.supr-con','http://export.arxiv.org/rss/cond-mat.supr-con','2013-01-01 00:00:00','2013-01-01 00:00:00','2013-01-01 00:00:00','http://arxiv.org/favicon.ico',NULL),
+	(11,'arXiv Strongly Correlated Electrons','arXiv:cond-mat.str-el','http://export.arxiv.org/rss/cond-mat.str-el','2013-01-01 00:00:00','2013-01-01 00:00:00','2013-01-01 00:00:00','http://arxiv.org/favicon.ico',NULL),
+	(12,'Nature Physics','Nature Physics','http://feeds.nature.com/nphys/rss/current','2013-01-01 00:00:00','2013-01-01 00:00:00','2013-01-01 00:00:00','http://www.nature.com/favicon.ico',NULL),
+	(13,'New Journal of Physics','NJP','http://iopscience.iop.org/1367-2630/?rss=1','2013-01-01 00:00:00','2013-01-01 00:00:00','2013-01-01 00:00:00',NULL,NULL),
+	(14,'Science','Science','http://www.sciencemag.org/rss/current.xml','2013-01-01 00:00:00','2013-01-01 00:00:00','2013-01-01 00:00:00',NULL,NULL);
+
+/*!40000 ALTER TABLE `journals` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table journals_categories
+# ------------------------------------------------------------
+
+LOCK TABLES `journals_categories` WRITE;
+/*!40000 ALTER TABLE `journals_categories` DISABLE KEYS */;
+
+INSERT INTO `journals_categories` (`journal_id`, `category_id`)
+VALUES
+	(1,1),
+	(12,1),
+	(13,1),
+	(14,1),
+	(2,1),
+	(3,5);
+
+/*!40000 ALTER TABLE `journals_categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table paths
+# ------------------------------------------------------------
+
+LOCK TABLES `paths` WRITE;
+/*!40000 ALTER TABLE `paths` DISABLE KEYS */;
+
+INSERT INTO `paths` (`id`, `journal_id`, `type`, `path`)
+VALUES
+	(3,1,'abstract','//div[@class=\"aps-abstractbox\"]/p'),
+	(4,2,'abstract','//*[@id=\"abstract\"]/div/p'),
+	(5,2,'abstract','//*[@id=\"first-paragraph\"]/p'),
+	(6,3,'abstract','//*[@id=\"abs\"]/div[2]/blockquote'),
+	(7,4,'abstract','//*[@id=\"abs\"]/div[2]/blockquote'),
+	(8,5,'abstract','//*[@id=\"abs\"]/div[2]/blockquote'),
+	(9,6,'abstract','//*[@id=\"abs\"]/div[2]/blockquote'),
+	(10,7,'abstract','//*[@id=\"abs\"]/div[2]/blockquote'),
+	(11,8,'abstract','//*[@id=\"abs\"]/div[2]/blockquote'),
+	(12,9,'abstract','//*[@id=\"abs\"]/div[2]/blockquote'),
+	(13,10,'abstract','//*[@id=\"abs\"]/div[2]/blockquote'),
+	(14,11,'abstract','//*[@id=\"abs\"]/div[2]/blockquote'),
+	(15,14,'abstract','//*[@id=\"abstract-3\"]'),
+	(16,13,'abstract','//*[@id=\"articleAbsctract\"]'),
+	(17,2,'abstract','//*[@id=\"first-paragraph\"]/p[2]');
+
+/*!40000 ALTER TABLE `paths` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
