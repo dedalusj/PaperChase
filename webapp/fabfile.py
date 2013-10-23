1806921e5f4a5cdb6b8d8fe26de824801df8d818
Jacopo Sabbatini <dedalusj@gmail.com>
2013-10-24 10:31:46 +1100

    Fixed bugs in fabfile

--- webapp/fabfile.py
+++ webapp/fabfile.py
@@ -26,6 +26,7 @@ env.mail_server = 'smtp.gmail.com'
 env.mail_port = '465'
 env.mail_use_ssl = 'True'
 env.mail_username = ADMIN_EMAIL
+env.virtual_env = 'venv'
 
 # Settings specific to the deploy environment   
 def vm():
@@ -58,17 +59,16 @@ def update_repo():
 def setup_venv():
     with cd(env.web_app_path):
         run("curl -O https://raw.github.com/pypa/virtualenv/1.9.X/virtualenv.py")
-        run("python virtualenv.py venv")
-        run("venv/bin/pip install -r requirements.txt")
-    fix_feedparser(path)
+        run("python virtualenv.py {0}".format(env.virtual_env))
+        run("{0}/bin/pip install -r requirements.txt".format(env.virtual_env))
+    fix_feedparser()
         
 def fix_feedparser():
     with cd(env.web_app_path):
         run("mkdir temp")
-        cd("temp")
-        run("git clone https://github.com/dedalusj/feedparser.git .")
-        run("mv feedparser/feedparser.py ../venv/lib/python2.7/")
-        cd("..")
+        with cd("temp"):
+            run("git clone https://github.com/dedalusj/feedparser.git .")
+            run("mv feedparser/feedparser.py ../venv/lib/python2.7/")
         run("rm -fdr temp")
         
 def prompt_for_settings():
@@ -93,7 +93,7 @@ def setup_settings():
 
 def update_database():
     with cd(env.web_app_path):
-        run("venv/bin/alembic upgrade head")
+        run("{0}/bin/alembic upgrade head".format(env.virtual_env))
 
 def copy_local_database():
     local('mysqldump -u {0} -p {1} > database_copy.sql'.format(env.db_username, env.local_database))
@@ -110,15 +110,15 @@ def setup_supervisor():
         supervisor_conf = StringIO.StringIO()
         supervisor_conf.write(_render_template(SUPERVISOR_CONF_FILE, env))
         put(supervisor_conf, SUPERVISOR_CONF_FILE)
-        run("venv/bin/supervisord")
+        run("{0}/bin/supervisord".format(env.virtual_env))
         
 def start_worker():
     with cd(env.web_app_path):
-        run("venv/bin/supervisorctl start celeryworker")
+        run("{0}/bin/supervisorctl start celeryworker".format(env.virtual_env))
         
 def stop_worker():
     with cd(env.web_app_path):
-        run("venv/bin/supervisorctl stop celeryworker")
+        run("{0}/bin/supervisorctl stop celeryworker".format(env.virtual_env))
         
 def run_redis():
     with settings(warn_only=True):
@@ -126,11 +126,11 @@ def run_redis():
         
 def start_app():
     with cd(env.web_app_path):
-        run("venv/bin/supervisorctl start paperchase")
+        run("{0}/bin/supervisorctl start paperchase".format(env.virtual_env))
 
 def stop_app():
     with cd(env.web_app_path):
-        run("venv/bin/supervisorctl stop paperchase")
+        run("{0}/bin/supervisorctl stop paperchase".format(env.virtual_env))
         
 def initial_setup(local_data = 'False'):
     make_dir()
