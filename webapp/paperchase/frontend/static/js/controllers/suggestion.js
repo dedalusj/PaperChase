e5bee2a3142c489f2719d2584b6a71e8cfa9f66b
Jacopo Sabbatini <dedalusj@gmail.com>
2013-11-06 22:21:32 +1000

    Fixed some problems with the suggestion form

--- webapp/paperchase/frontend/static/js/controllers/suggestion.js
+++ webapp/paperchase/frontend/static/js/controllers/suggestion.js
@@ -15,23 +15,7 @@ app.controller("suggestionController", ['$scope', 'CategoryAPI', '$http', functi
         $scope.subcategories = CategoryAPI.getSubcategories({'category_id' : $scope.category.id});
     };
     
-    $scope.formatAndSubmit = function(suggestion) {
-        if (suggestion.$valid) {
-            var postData = {name: $scope.name, 
-                            url: $scope.url,
-                            category: $scope.category.name,
-                            subcategory: $scope.subcategory.name,
-                            title: $scope.title,
-                            authors: $scope.authors,
-                            abstract: $scope.abstract};
-            $http({
-                url: '/api/suggestion',
-                method: "POST",
-                data: angular.toJson(postData),
-                headers: {'Content-Type': 'application/json'}
-            }).success(function() {
-                $scope.alert = { type: 'success', msg: 'Your journal suggestion has been submitted!' };
-            })
-        }
+    $scope.submitted = function() {
+        $scope.alert = { type: 'success', msg: 'The developers will review the information in your email, add the journal and contact you. You can now return to the <a href="#/home">home page</a>' };
     };
 }]);
