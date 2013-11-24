290fdd3e71d46c3595d0cdec58bd6c572b193151
Jacopo Sabbatini <dedalusj@gmail.com>
2013-11-24 21:54:33 +1000

    New registration alert message

--- webapp/paperchase/frontend/static/js/controllers/register.js
+++ webapp/paperchase/frontend/static/js/controllers/register.js
@@ -12,7 +12,7 @@ app.controller("registerController", ['$scope', '$http', '$location', 'UserServi
             data: angular.toJson(postData),
             headers: {'Content-Type': 'application/json'}
         }).success(function() {
-            $scope.alert = { type: 'success', msg: 'Congratulations! You will receive a confirmation email shortly. <a href="#/login">Login here</a>' };
+            $scope.alert = { type: 'success', msg: 'Congratulations! You will receive a confirmation email shortly. You can login <a href="#/login">here</a>' };
         }).error(function(data, status, headers, config) {
             if (status == 409) {
                 $scope.alert = { type: 'danger', msg: 'The email address is already registered.' };
