8d04aba488b6c38abd1fd09e4ed5cef15b1d2a17
Jacopo Sabbatini <dedalusj@gmail.com>
2013-12-07 23:29:22 +1000

    modified the httpProvider interceptors in the frontend app
    Moved away from a deprecated implementation of the interceptor and also
    taking advantage of the new backend header trick to return 403 instead
    of 401 for failed auth

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -9,48 +9,49 @@ angular.module('paperchaseApp', [
     'ui.bootstrap',
     'ngBase64'
 ])
-    .config(['$routeProvider', '$locationProvider', '$httpProvider', '$compileProvider',
-            function ($routeProvider, $locationProvider, $httpProvider, $compileProvider) {
-            $routeProvider
-                .when('/', {
-                    templateUrl: 'views/main.html',
-                    controller: 'MainCtrl'
-                })
-                .when('/subscriptions', {
-                    templateUrl: 'views/subscriptions.html',
-                    controller: 'SubscriptionsCtrl'
-                })
-                .when('/login', {
-                    templateUrl: 'views/login.html',
-                    controller: 'LoginCtrl'
-                })
-                .when('/register', {
-                    templateUrl: 'views/register.html',
-                    controller: 'RegisterCtrl'
-                })
-                .when('/suggestion', {
-                    templateUrl: 'views/suggestion.html',
-                    controller: 'SuggestionCtrl'
-                })
-                .otherwise({
-                    redirectTo: '/'
-                });
+.config(['$routeProvider', '$locationProvider', '$httpProvider', '$compileProvider',
+    function ($routeProvider, $locationProvider, $httpProvider, $compileProvider) {
+        $routeProvider
+        .when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl'
+        })
+        .when('/subscriptions', {
+            templateUrl: 'views/subscriptions.html',
+            controller: 'SubscriptionsCtrl'
+        })
+        .when('/login', {
+            templateUrl: 'views/login.html',
+            controller: 'LoginCtrl'
+        })
+        .when('/register', {
+            templateUrl: 'views/register.html',
+            controller: 'RegisterCtrl'
+        })
+        .when('/suggestion', {
+            templateUrl: 'views/suggestion.html',
+            controller: 'SuggestionCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
 
-            var interceptor = ['$location', '$q', function ($location, $q) {
-                    function success(response) {
-                        return response;
+        $httpProvider.interceptors.push(['$q', '$location', function($q, $location) {
+            return {
+                'request': function (config) {
+                    config.headers['X-StatusOnLoginFail'] = '403';
+                    return config;
+                },
+                'responseError': function (rejection) {
+                    if (rejection.status === 403) {
+                        $location.path('/login');
+                        return $q.reject(rejection);
                     }
-                    function error(response) {
-                        if (response.status === 401) {
-                            $location.path('/login');
-                            return $q.reject(response);
-                        }
-                        return $q.reject(response);
-                    }
-                    return function (promise) {
-                        return promise.then(success, error);
-                    };
-                }];
-            $httpProvider.responseInterceptors.push(interceptor);
-            $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|papers):/);
+                    return $q.reject(rejection);
+                }
+            };
         }]);
+
+
+        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|papers):/);
+    }]);
