478ac08a003300e62685e999d18f2ba910afd972
Jacopo Sabbatini <dedalusj@gmail.com>
2013-12-08 12:12:18 +1000

    Implemented new subscription page with AngularJS custom filter and new API

--- frontend/app/scripts/services/CategoryAPI.js
+++ frontend/app/scripts/services/CategoryAPI.js
@@ -5,7 +5,6 @@ angular.module('paperchaseApp')
     .factory('CategoryAPI', ['$http', '$resource', function ($http, $resource) {
         return $resource('/api/categories/:categoryId/:resource', {}, {
             'getCategories' : { method : 'GET', isArray : true, cache : true},
-            'getSubcategories' : { method : 'GET', params: { categoryId : '@id', resource : 'subcategories' }, isArray : true, cache : true },
-            'getJournals' : { method : 'GET', params : { categoryId : '@id', resource : 'journals' }, isArray : true, cache : true }
+            'getSubcategories' : { method : 'GET', params: { categoryId : '@id', resource : 'subcategories' }, isArray : true, cache : true }
         });
     }]);
