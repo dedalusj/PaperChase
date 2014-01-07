23296148adc27c26668f135e8a276b266bda5cf9
Jacopo Sabbatini <dedalusj@gmail.com>
2014-01-07 13:33:28 +1000

    Added method to fetch the papers for a given journal to the PaperAPI front-end

--- frontend/app/scripts/services/PaperAPI.js
+++ frontend/app/scripts/services/PaperAPI.js
@@ -6,6 +6,10 @@ angular.module('paperchaseApp')
         // Define the resource for the paper API to be shared by all other services and controllers
         return $resource('/api/papers/:paperId', {}, {
             'getPapers' : { method : 'GET', isArray : true},
+            'getPapersForJournal' : { url : '/api/journals/:journalId/papers',
+                                      params : { journalId : '@id'},
+                                      method : 'GET',
+                                      isArray : true},
             'getPaper' : { method : 'GET', params: { paperId : '@id' }},
             'getUnreadList' : { url : '/api/unread_papers', method : 'GET', isArray : true},
             'markUnread' : { url : '/api/unread_papers', method : 'PUT', isArray : true },
